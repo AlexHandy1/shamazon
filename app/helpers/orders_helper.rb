f0e00a19172211eef48753cc88ff7ea9d933f24c
Alex Handy <handy437@gmail.com>
2015-08-02 11:19:16 +0100

    Use Case 5,6,7: Apply vouchers, view total with discounts and invalid voucher alert

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,20 @@
 module OrdersHelper
+  def check_voucher(order, categories, voucher)
+    if voucher == "Voucher 1"
+      order.apply_discount_one
+      order.update_total
+      redirect_to :back
+    elsif voucher == "Voucher 2" && order.total >= 50
+      order.apply_discount_two
+      order.update_total
+      redirect_to :back
+    elsif voucher == "Voucher 3" && order.total >= 75 && (categories.include?("Men’s Footwear") || categories.include?("Women’s Footwear"))
+      order.apply_discount_three
+      order.update_total
+      redirect_to :back
+    else
+      flash[:notice] = "Invalid Voucher"
+      redirect_to :back
+    end
+  end
 end
